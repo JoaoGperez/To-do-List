60431589de336eed5e00881462df02f82b6fb511
João Gabriel Perez <jgpmgin31@gmail.com>
2025-04-09 13:24:29 -0300

    upload task controller

---
+++ roteiro1/roteiro1/src/main/java/com/labdessoft/roteiro1/controller/TaskController.java
@@ -0,0 +1,12 @@
+package com.labdessoft.roteiro1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+    @GetMapping("/task")
+    public String listAll(){
+        return "Retornar tosas as tasks";
+    }
+}
